5bf526e788b62a1d54c0514404a9009db042a17d
Tomas Kosegi <tomas.kosegi@gmail.com>
2018-03-26 00:05:07 +0200

    random results

--- communication/client.py
+++ communication/client.py
@@ -6,6 +6,15 @@ import json
 
 url = 'http://127.0.0.1:8000/running-test'
 tn = 5
+
+def result_generator():
+	coin = random.randint(0,10)
+	
+	if coin < 3:
+		return 'passed'
+	else:
+		return 'failed'
+
 for i in range(1, tn+1):
 	test = {}
 	test['id'] = int(uuid.uuid4())
@@ -16,7 +25,7 @@ for i in range(1, tn+1):
 	requests.post(url, data=json.dumps(test))
 		
 	for j in range(1, test['steps']+1):	
-		(test['results']).append({'step': j, 'passed': 'ok'})
+		(test['results']).append({'step': j, 'result': result_generator()})
 		requests.post(url , data=json.dumps(test))
 		print('test no {}/{}  step {}/{}'.format(i, tn, j, test['steps'] ))   
 		time.sleep(random.randint(1,10))
