c883d4caab7cfcd82f3df8481b818304f1a2014c
MAIN\SK040122 <Tomas.Kosegi@anritsu.com>
2018-03-26 17:46:42 +0200

    server client

---
+++ communication/client0.py
@@ -0,0 +1,44 @@
+import requests
+import uuid
+import random
+import time
+import json
+
+# url = 'http://127.0.0.1:8000'
+url = 'http://BTS-826GLV1.main.intgin.net:8000'
+runningUrl = '{}/test/running'.format(url)
+print(runningUrl)
+tn = 5
+
+def result_generator():
+	coin = random.randint(0,10)
+
+	if coin < 6:
+		return 'passed'
+	else:
+		return 'failed'
+
+for i in range(1, tn+1):
+	test = {}
+	test['id'] = int(uuid.uuid4())
+	test['status'] = 'running'
+	test['steps'] = random.randint(4,8)
+	test['results'] = []
+
+	requests.post(runningUrl, data=json.dumps(test))
+
+	for j in range(1, test['steps']+1):
+		(test['results']).append({'step': j, 'result': result_generator()})
+		requests.post(runningUrl , data=json.dumps(test))
+		print('test no {}/{}  step {}/{}'.format(i, tn, j, test['steps'] ))
+		time.sleep(random.randint(1,10))
+
+
+
+	test['status'] = 'done'
+	doneUrl = '{}/test/done/{}'.format(url, test['id'])
+	print(doneUrl)
+	requests.post(doneUrl , data=json.dumps(test))
+	print(test)
+
+print('done')
--- communication/server.py
+++ communication/server.py
@@ -5,14 +5,27 @@ import os
 import os.path as path
 
 absPath = path.dirname(path.abspath(__file__))
-testDir = 'archive'
-pathToArchive = path.join(absPath, testDir)
+archiveDir = 'archive'
+runningDir = 'running'
+pathToArchive = path.join(absPath, archiveDir)
+pathToRunning = path.join(absPath, runningDir)
 
-if path.isdir(path.join(absPath, testDir)):
+
+if path.isdir(pathToArchive):
 	print('Directory archive  exists')
 else:
 	print('create directory {}'.format(pathToArchive))
 	os.makedirs(pathToArchive, exist_ok=True)
+
+if path.isdir(pathToRunning):
+	print('Directory running exists')
+else:
+	print('create directory {}'.format(pathToRunning))
+	os.makedirs(pathToRunning, exist_ok=True)
+
+running_test_file = '{}/running.json'.format(pathToRunning, 'running')
+with open(running_test_file, 'w') as f:
+	json.dump({'status': 'none', 'id': 'none'}, f)
 
 def isRunning(data):
 	# data = json.loads(data)
@@ -46,6 +59,29 @@ class RunningTestResource:
 		else:
 			print('Sendt data - no running test')
 
+class RunningTestResource2:
+	def on_post(self, req, resp):
+		with open(running_test_file, 'w') as f:
+			json.dump(req.media, f)
+		print('Running test updated {}'.format(req.media))
+
+	def on_get(self, req, resp):
+		with open(running_test_file, 'r') as f:
+			data = json.load(f)
+			resp.body = json.dumps(data, ensure_ascii=False)
+		resp.status = falcon.HTTP_200
+		print('Sent data - running test')
+
+class RunningTestDoneResource:
+	def on_post(self, req, resp, id):
+		with open(running_test_file, 'w') as f:
+			json.dump({'status': 'none', 'id': 'none'}, f)
+		file_name = '{}/{}.json'.format(pathToArchive, id)
+		print('Test done - remove running')
+		with open(file_name, 'w') as f:
+			json.dump(req.media, f)
+		print('Test done - added to archive')
+
 class TestsResource:
 	def on_get(self, req, resp):
 		resp.status = falcon.HTTP_200
@@ -63,8 +99,10 @@ class TestFromTestsResource:
 		print('Sent data - test {}'.format(id))
 
 api = application = falcon.API()
-api.add_route('/tests/running', RunningTestResource())
+api.add_route('/running-test', RunningTestResource())
 api.add_route('/tests/archive/', TestsResource())
 api.add_route('/tests/archive/{id}', TestFromTestsResource())
+api.add_route('/test/running', RunningTestResource2())
+api.add_route('/test/done/{id}', RunningTestDoneResource())
 # tests/running/{id}
 # tests/running/done/{id}
