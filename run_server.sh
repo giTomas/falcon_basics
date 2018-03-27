a2a2512d50a7c3b6fa72e8bb9f8bf225939c1db0
MAIN\SK040122 <Tomas.Kosegi@anritsu.com>
2018-03-27 16:37:46 +0200

    add check to run_server script

--- run_server.sh
+++ run_server.sh
@@ -1,1 +1,9 @@
-gunicorn --reload comminication.server
+#https://stackoverflow.com/questions/592620/check-if-a-program-exists-from-a-bash-script
+
+if [ -x "$(command -v gunnicorn)"]
+then
+    gunicorn --reload comminication.server
+else
+    echo "gunicorn not installed"
+    echo "sudo pip install gunicorn"
+fi
