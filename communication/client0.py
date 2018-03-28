8558c50cdd0fdf6ca78750c980e468a309df6b55
Tomas Kosegi <tomas.kosegi@gmail.com>
2018-03-28 23:47:25 +0200

    small changes

--- communication/client0.py
+++ communication/client0.py
@@ -4,39 +4,44 @@ import random
 import time
 import json
 
-url = 'http://127.0.0.1:8000'
-#url = 'http://BTS-826GLV1.main.intgin.net:8000'
-runningUrl = '{}/test/running'.format(url)
-tn = 5
+def main(
+	url='http://127.0.0.1:8000',
+	tn=5):
 
-def result_generator():
-	coin = random.randint(0,10)
+	#url = 'http://BTS-826GLV1.main.intgin.net:8000'
+	runningUrl = '{}/test/running'.format(url)
 
-	if coin < 6:
-		return 'passed'
-	else:
-		return 'failed'
+	def result_generator():
+		coin = random.randint(0,10)
 
-for i in range(1, tn+1):
-	test            = {}
-	test['id']      = int(uuid.uuid4())
-	test['status']  = 'running'
-	test['steps']   = random.randint(4,8)
-	test['results'] = []
+		if coin < 6:
+			return 'passed'
+		else:
+			return 'failed'
 
-	requests.post(runningUrl, data=json.dumps(test))
+	for i in range(1, tn+1):
+		test            = dict()
+		test['id']      = int(uuid.uuid4())
+		test['status']  = 'running'
+		test['steps']   = random.randint(4,8)
+		test['results'] = []
 
-	for j in range(1, test['steps']+1):
-		(test['results']).append({'step': j, 'result': result_generator()})
-		requests.post(runningUrl , data=json.dumps(test))
-		print('test no {}/{}  step {}/{}'.format(i, tn, j, test['steps'] ))
-		time.sleep(random.randint(1,10))
+		requests.post(runningUrl, data=json.dumps(test))
+
+		for j in range(1, test['steps']+1):
+			(test['results']).append({'step': j, 'result': result_generator()})
+			requests.post(runningUrl , data=json.dumps(test))
+			print('test no {}/{}  step {}/{}'.format(i, tn, j, test['steps'] ))
+			time.sleep(random.randint(1,10))
 
 
+		test['status'] = 'done'
+		doneUrl = '{}/test/done/{}'.format(url, test['id'])
+		requests.post(doneUrl , data=json.dumps(test))
+		print(test)
 
-	test['status'] = 'done'
-	doneUrl = '{}/test/done/{}'.format(url, test['id'])
-	requests.post(doneUrl , data=json.dumps(test))
-	print(test)
+	print('done')
 
-print('done')
+
+if __name__ == '__main__':
+	main()
--- run_server.sh
+++ run_server.sh
@@ -1,11 +1,10 @@
 
 #https://stackoverflow.com/questions/592620/check-if-a-program-exists-from-a-bash-script
 
-if [ -x "$(command -v gunicorn)" ]
-then
+if [ -x "$(command -v gunicorn)" ]; then
     gunicorn --reload communication.server
 else
-    echo "gunicorn not installed"
+    echo "Gunicorn server not installed"
     echo "sudo pip install gunicorn"
 fi
 
